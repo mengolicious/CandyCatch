31211e73d34ae2ca57e1ae8cad44e444e67b5d4b
Lord-Mordicai <jmartin2195@gmail.com>
2017-04-19 14:44:35 +1000

    Bubble ready to be implimented into a spawning system
    Bubbles will need to be given a colour at a later stage

--- Assets/Scripts/BackgroundBubble_Script.cs
+++ Assets/Scripts/BackgroundBubble_Script.cs
@@ -4,21 +4,60 @@ using System.Collections;
 public class BackgroundBubble_Script : MonoBehaviour
 {
 	private Animator anim;
-
+	private float targetScale;
+	private bool Poppable;
 	void Start()
 	{
 		anim = GetComponent<Animator>();
+		Poppable = false;
+		transform.localScale = Vector3.zero;
+	}
+
+	public void InitialiseVariables(Vector3 position, float intendedScale)
+	{
+		transform.position = position;
+		targetScale = intendedScale;
+		Poppable = false;
+		transform.localScale = Vector3.zero;
+		StartCoroutine(ExpandBubble());
+	}
+
+	IEnumerator ExpandBubble()
+	{
+		while(!Poppable)
+		{
+			if(transform.localScale.x != targetScale)
+			{
+				transform.localScale += Vector3.one * 0.1f;
+			}
+			yield return new WaitForSeconds(0.3f);
+		}
 	}
 
 	public void OnMouseOver()
 	{
-		//if()
+		if(anim.GetCurrentAnimatorStateInfo(0).IsName("Idle") && Poppable)
 		{
 			if(Input.GetMouseButtonDown(0))
 			{
-				anim.Play("Burst");
+				anim.Play("BubbleBurst");
+				StartCoroutine(SelfDestroy());
 				//JUST DO IT, DON'T LET YOUR DREAMS JUST BE DREAMS - SHIA LABEOUF
 			}
+		}
+	}
+
+	IEnumerator SelfDestroy()
+	{
+		float waitTime = 0.6f;
+		while(true)
+		{
+			waitTime -= 0.03f;
+			if(waitTime < 0f)
+			{
+				Destroy(gameObject);
+			}
+			yield return new WaitForSeconds(0.03f);
 		}
 	}
 }
