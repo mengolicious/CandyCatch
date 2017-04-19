9460b77010c2e48e63aead78310b28cbf8ac438c
Lord-Mordicai <jmartin2195@gmail.com>
2017-04-19 11:42:16 +1000

    Beginings of bubble interactables

---
+++ Assets/Scripts/BackgroundBubble_Script.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BackgroundBubble_Script : MonoBehaviour
+{
+	private Animator anim;
+
+	void Start()
+	{
+		anim = GetComponent<Animator>();
+	}
+
+	public void OnMouseOver()
+	{
+		//if()
+		{
+			if(Input.GetMouseButtonDown(0))
+			{
+				anim.Play("Burst");
+				//JUST DO IT, DON'T LET YOUR DREAMS JUST BE DREAMS - SHIA LABEOUF
+			}
+		}
+	}
+}
