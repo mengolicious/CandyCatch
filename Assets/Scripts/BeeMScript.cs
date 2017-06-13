e1890ff6f6cd2968fa19e121c22643327586a4ee
Lord-Mordicai <jmartin2195@gmail.com>
2017-06-13 16:23:19 +1000

    Wip changes to BeeM to handle bonus mode bees
    Probably make Bonus mode bees to be their own new for of bee

--- Assets/Scripts/BeeMScript.cs
+++ Assets/Scripts/BeeMScript.cs
@@ -83,11 +83,16 @@ public class BeeMScript : MonoBehaviour
 		StartCoroutine(ReleaseTheBees());
 	}
 
-	// Update is called once per frame
-	/*void Update () {
-	
-	}*/
+	public void SwitchToBonusRound()
+	{
+		//load the prefab for the bonus round bees, whatever they bee called cap'n
+		BeePrefab = Resources.Load("prefabs/BonusRoundBees");
+	}
 
+	public void SpawnBeeWave()
+	{
+		StartCoroutine(BeeWaveSpawner());
+	}
 	/// <summary>
 	/// Spawns the bees with a delay between each, number spawned based off of member variable.
 	/// </summary>
@@ -97,13 +102,13 @@ public class BeeMScript : MonoBehaviour
 		GameObject tempQueenBee;
 		int tI;
 		//Debug.Log ("Spawning " + numberOfBEES + " Bees");
-		for(int x = 0; x < numberOfBEES; x++)
+		for (int x = 0; x < numberOfBEES; x++)
 		{
 			//beeValue = Random.Range(1, maxBeeValue+1);
-			tI = Random.Range(0,SpawnPoints.Count);
+			tI = Random.Range(0, SpawnPoints.Count);
 			//Vector3 shiftPos = new Vector3(0f, Random.Range (-2.5f,2.5f), 0f);
 			tempBee = GameObject.Instantiate(BeePrefab, SpawnPoints[tI], Quaternion.identity) as GameObject;
-			tempBee.GetComponent<Bee_Script>().InitialiseVariables(tempBall, beeSpeed, beeValue,BeeBurstPrefab,ScoreChangeSpritePrefab, Hive.transform.position, this);
+			tempBee.GetComponent<Bee_Script>().InitialiseVariables(tempBall, beeSpeed, beeValue, BeeBurstPrefab, ScoreChangeSpritePrefab, Hive.transform.position, this);
 			//tempBee.GetComponent<Bee_Script>().value = tempValue;
 			beeList.Add(tempBee);
 			UsedSpawnPoints.Add(SpawnPoints[tI]);
@@ -112,12 +117,13 @@ public class BeeMScript : MonoBehaviour
 		}
 
 		//---------------------------Start of Queen Bee--------------------------//
-		if(isExpert){
-			Debug.Log ("The Queen cometh");
-			tI = Random.Range(0,SpawnPoints.Count);
+		if (isExpert)
+		{
+			Debug.Log("The Queen cometh");
+			tI = Random.Range(0, SpawnPoints.Count);
 			//Vector3 shiftPos = new Vector3(0f, Random.Range (-2.5f,2.5f), 0f);
 			tempQueenBee = GameObject.Instantiate(QueenBeePrefab, SpawnPoints[tI], Quaternion.identity) as GameObject;
-			tempQueenBee.transform.GetChild(0).GetComponent<QueenBeeScript>().InitialiseVariables(tempBall, queenBeeSpeed, beeValue,BeeBurstPrefab,ScoreChangeSpritePrefab, Hive.transform.position, this);
+			tempQueenBee.transform.GetChild(0).GetComponent<QueenBeeScript>().InitialiseVariables(tempBall, queenBeeSpeed, beeValue, BeeBurstPrefab, ScoreChangeSpritePrefab, Hive.transform.position, this);
 			//tempBee.GetComponent<Bee_Script>().value = tempValue;
 			//beeList.Add(tempBee); //add this back ***
 			UsedSpawnPoints.Add(SpawnPoints[tI]);
@@ -125,6 +131,26 @@ public class BeeMScript : MonoBehaviour
 
 		}
 		//---------------------------END of Queen Bee--------------------------//
+	}
+
+	IEnumerator BeeWaveSpawner()
+	{
+		GameObject tempBee;
+		int tI;
+		//Debug.Log ("Spawning " + numberOfBEES + " Bees");
+		for (int x = 0; x < 4; x++)
+		{
+			//beeValue = Random.Range(1, maxBeeValue+1);
+			tI = Random.Range(0, SpawnPoints.Count);
+			//Vector3 shiftPos = new Vector3(0f, Random.Range (-2.5f,2.5f), 0f);
+			tempBee = GameObject.Instantiate(BeePrefab, SpawnPoints[tI], Quaternion.identity) as GameObject;
+			tempBee.GetComponent<Bee_Script>().InitialiseVariables(tempBall, beeSpeed, beeValue, BeeBurstPrefab, ScoreChangeSpritePrefab, Hive.transform.position, this);
+			//tempBee.GetComponent<Bee_Script>().value = tempValue;
+			beeList.Add(tempBee);
+			UsedSpawnPoints.Add(SpawnPoints[tI]);
+			SpawnPoints.RemoveAt(tI);
+			yield return null;
+		}
 	}
 
 	public bool BeesAlive()
@@ -144,7 +170,14 @@ public class BeeMScript : MonoBehaviour
 	/// </summary>
 	public void ClearBees()
 	{
-		StartCoroutine(ClearListCoRoutine());
+		if (SVM_Script.Instance.isBonus)
+		{
+			StartCoroutine(ClearBonusListCoRoutine());
+		}
+		else
+		{
+			StartCoroutine(ClearListCoRoutine());
+		}
 	}
 
 	public void HiveShake(int BeeValue)
@@ -199,7 +232,6 @@ public class BeeMScript : MonoBehaviour
 				beeList[x].GetComponent<Bee_Script>().ClearTarget();
 			yield return null;
 		}
-		//beeList.Clear();
 		SpawnPoints.Clear();
 		UsedSpawnPoints.Clear();
 		for( int x = 0; x < 5; ++x)
@@ -208,8 +240,26 @@ public class BeeMScript : MonoBehaviour
 		}
 	}
 
+	IEnumerator ClearBonusListCoRoutine()
+	{
+		for (int x = 0; x < beeList.Count; x++)
+		{
+			//if (!beeList[x])
+			//	beeList.RemoveAt(x);
+			//else
+			//	beeList[x].GetComponent<Bee_Script>().ClearTarget();
+			yield return null;
+		}
+		SpawnPoints.Clear();
+		UsedSpawnPoints.Clear();
+		//for (int x = 0; x < 5; ++x)
+		//{
+		//	SpawnPoints.Add(transform.GetChild(x).position);
+		//}
+	}
+
 	public void DestroyOthers()
 	{
-		Destroy (this.Hive);
+		Destroy (Hive);
 	}
 }
